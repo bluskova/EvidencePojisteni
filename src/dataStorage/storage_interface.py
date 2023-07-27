40509e883b3c6096060068ab904dc9267187f5a8
bluskova <barbora.luskova@gmail.com>
2023-07-27 10:28:27 +0200

    ABC

--- src/dataStorage/storage_interface.py
+++ src/dataStorage/storage_interface.py
@@ -1,9 +1,9 @@
-from abc import abstractmethod
+from abc import abstractmethod, ABC
 from typing import Optional, List
 from src.person import Person
 
 
-class StorageInterface:
+class StorageInterface(ABC):
     """
     Rozhradní pro práci s databází.
     """
